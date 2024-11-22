cd96cd876dbba3626c4495e17b4b9ef8c56c834d
olivalivava <oliva.huang@outlook.com>
2024-11-22 23:14:02 +0000

    added fcorr

--- Code/routines.py
+++ Code/routines.py
@@ -574,7 +574,7 @@ def read_settings(filename):
     av['cv'] = av['cp'] / av['gam']
 
     # Read the CFL, smoothing factor and convergence limit
-    av['cfl'],av['sfac'],av['d_max'] = [float(x) for x in f.readline().split()]
+    av['cfl'],av['sfac'],av['d_max'], av['fcorr']= [float(x) for x in f.readline().split()]
 
     # Read the number of steps
     av['nsteps'] = [int(x) for x in f.readline().split()]
